78d832b01f9348fbf5400688dee7df1b8d20e326
Alireza <alirezarezaei8080@gmail.com>
2022-08-01 12:35:32 +0430

    basic user interface added

---
+++ Game_Of_Life/Web/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function (){
+    for(let i=0;i<40;i++){
+        $("tbody").append(` <tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>`);
+    }
+});
