e0ee31bf9d952b39d96761fe341e8fea4ab08356
Alireza <alirezarezaei8080@gmail.com>
2022-08-10 16:24:39 +0430

    room joining functionality added

--- P2P_VideoCall/client/home.js
+++ P2P_VideoCall/client/home.js
@@ -1,10 +1,25 @@
+var socket = io();
 var video = document.querySelector("#videoplayer");
 
 if (navigator.mediaDevices.getUserMedia){
-    
     navigator.mediaDevices.getUserMedia({video:true}).then((stream) => {
+        var room = prompt("Enter a room:");
+        while (room === '' || room === null){
+            room = prompt("Enter a room:");
+        }
+        socket.emit("requestroom",{"room":room});
         video.srcObject = stream;
     }).catch((err) => {
         console.log("some error happended with fetching video feed");
     });
 }
+
+socket.on("roomerror", () =>{
+    alert("Room full or invalid!");
+    room = prompt("Enter a room:");
+    while (room === '' || room === null){
+        room = prompt("Enter a room:");
+    }
+    socket.emit("requestroom",{"room":room});
+});
+
--- P2P_VideoCall/main.js
+++ P2P_VideoCall/main.js
@@ -2,11 +2,25 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(express.static("client"));
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/client/" + "home.html");
+});
+
+io.on("connection", (socket) =>{
+    console.log("connected to socket!");
+
+    socket.on("requestroom", (data) =>{
+        if (! io.of("/").adapter.rooms.has(data["room"]) || (io.of("/").adapter.rooms.has(data["room"]) && io.of("/").adapter.rooms.get(data["room"]).size < 2)){
+            socket.join(data["room"]);
+        } else {
+            socket.emit("roomerror");
+        }
+    });
 });
 
 server.listen(3000, () =>{
